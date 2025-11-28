1d75dea86a9bf31f3c984414080dd5149faeb6a0
DevanandGit <nandhudevanand4419@gmail.com>
2025-11-28 19:14:38 +0530

    migrations corrected

--- prisma/migrations/20251127060956_relation_b_w_service_and_meetings/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `Meetings` ADD COLUMN `serviceId` VARCHAR(191) NULL;
-
--- AddForeignKey
-ALTER TABLE `Meetings` ADD CONSTRAINT `Meetings_serviceId_fkey` FOREIGN KEY (`serviceId`) REFERENCES `Service`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
--- prisma/migrations/20251127101003_incerased_column_size_of_service_modle/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `Service` MODIFY `description` TEXT NULL;
