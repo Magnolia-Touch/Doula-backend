dfad1843398fdf6b4b5aad78fb9247e0b1d41ca2
DevanandGit <nandhudevanand4419@gmail.com>
2026-01-01 19:10:20 +0530

    conflicts cleared 5

--- src/stripe/stripe.controller.ts
+++ src/stripe/stripe.controller.ts
@@ -6,8 +6,6 @@ import {
   HttpStatus,
   Logger,
   Get,
-  HttpCode,
-  BadRequestException,
 } from '@nestjs/common';
 import type { Request, Response } from 'express';
 import { WebhookService } from './webhook.service';
@@ -31,60 +29,35 @@ export class StripeController {
    * - Raw body must be enabled in main.ts
    * - No JSON parsing here
    */
-  // @Post('webhook')
-  // async handleStripeWebhook(
-  //   @Req() req: Request,
-  //   @Res() res: Response,
-  // ) {
-  //   console.log('Body is buffer:', Buffer.isBuffer(req.body));
-  //   console.log('Body length:', (req.body as Buffer)?.length);
-
-  //   const signature = req.headers['stripe-signature'] as string;
-
-  //   if (!signature) {
-  //     return res
-  //       .status(HttpStatus.BAD_REQUEST)
-  //       .send('Missing stripe-signature header');
-  //   }
-
-  //   try {
-  //     // ✅ USE req.body (Buffer)
-  //     const event = this.webhookService.verifyWebhookSignature(
-  //       req.body as Buffer,
-  //       signature,
-  //     );
-
-  //     await this.webhookService.processWebhookEvent(event);
-
-  //     return res.status(HttpStatus.OK).json({ received: true });
-  //   } catch (error) {
-  //     return res.status(HttpStatus.BAD_REQUEST).send(error.message);
-  //   }
-  // }
-
-
   @Post('webhook')
-  @HttpCode(200)
   async handleStripeWebhook(
-    @Req() req: Request & { rawBody?: Buffer },
+    @Req() req: Request,
+    @Res() res: Response,
   ) {
     console.log('Body is buffer:', Buffer.isBuffer(req.body));
     console.log('Body length:', (req.body as Buffer)?.length);
 
     const signature = req.headers['stripe-signature'] as string;
+
     if (!signature) {
-      throw new BadRequestException('Missing stripe-signature header');
+      return res
+        .status(HttpStatus.BAD_REQUEST)
+        .send('Missing stripe-signature header');
     }
 
-    const event = this.webhookService.verifyWebhookSignature(
-      req.rawBody!,
-      signature,
-    );
+    try {
+      // ✅ USE req.body (Buffer)
+      const event = this.webhookService.verifyWebhookSignature(
+        req.body as Buffer,
+        signature,
+      );
 
-    await this.webhookService.processWebhookEvent(event);
+      await this.webhookService.processWebhookEvent(event);
 
-    return { received: true };
+      return res.status(HttpStatus.OK).json({ received: true });
+    } catch (error) {
+      return res.status(HttpStatus.BAD_REQUEST).send(error.message);
+    }
   }
-
 
 }
